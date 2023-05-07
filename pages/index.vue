693bb0c1d7a2c2695adea0afc33e0e8ab65680d8
Olsen <krajaolsen@gmail.com>
2023-05-07 23:45:44 +0200

    Adding card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,42 @@
+<script setup>
+const selectedCardIndex = ref()
+const cards = ref([
+  {
+    number: 4,
+    color: 'hearts',
+  },
+  {
+    number: 6,
+    color: 'hearts',
+  },
+  {
+    number: 'J',
+    color: 'hearts',
+  },
+  {
+    number: 'K',
+    color: 'spades',
+  },
+])
+
+function select(index) {
+  selectedCardIndex.value = index
+}
+</script>
+
 <template>
   <h1 class="text-red-500 text-4xl">
     {{ $t('home') }}
   </h1>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolore ipsa neque nesciunt nulla quia quisquam,
-    sint temporibus unde. Aspernatur dolores ex minus molestiae nam perferendis quasi quia soluta voluptate.
-  </p>
+  <div class="flex ml-64">
+    <Card
+      v-for="(card, i) in cards"
+      :key="card"
+      :card="card"
+      :index="i"
+      :is-selected="i === selectedCardIndex"
+      :amount="cards.length"
+      @selected="select"
+    />
+  </div>
 </template>
